cdba58b508b35e17e4afa590af2e9f3e08740b10
DenisTis <tislenko.denis@gmail.com>
2017-10-06 20:05:22 +0200

    npm created with most libraries, user settings configured in VS code, webpack configured

---
+++ webpack.config.js
@@ -0,0 +1,18 @@
+var path = require("path");
+var webpack = require("webpack");
+
+module.exports = {
+  entry: "./src/index.js",
+  devtool: "inline-source-map",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "bundle.js",
+    publicPath: "/dist"
+  },
+  plugins: [
+    new webpack.ProvidePlugin({
+      THREE: "three",
+      CANNON: "cannon"
+    })
+  ]
+};
