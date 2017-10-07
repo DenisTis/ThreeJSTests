e87aea16dc0bb3a86ff03cd3b5241a02d47ed0c5
DenisTis <tislenko.denis@gmail.com>
2017-10-07 11:46:02 +0200

    final commit

--- README.md
+++ README.md
@@ -3,3 +3,14 @@ Tests of three.js project implementation
 
 Learning of three.js library and what could be achieved with it.
 
+1. Project setup.
+Workspace environment configured in VS Code.
+Webpack configured for single-page-application (test with command "npm run build")
+Project libraries (three.js, cannon.js and others added)
+Documentation generation added. To generate, use "documentation build src/** -f html -o docs"
+
+NEXT STEPS: 
+in next branch I will add REACT to have navigation to different views for single-page-application setup
+once working, I will try to implement internationalization with Intl
+
+Optionally, I will add webpack configuration for babel (old versions compatibility) and minify for production version
--- webpack.config.js
+++ webpack.config.js
@@ -1,18 +1,42 @@
 var path = require("path");
 var webpack = require("webpack");
+var IntlPlugin = require("intl-webpack-plugin");
 
 module.exports = {
-  entry: "./src/index.js",
-  devtool: "inline-source-map",
-  output: {
-    path: path.resolve(__dirname, "dist"),
-    filename: "bundle.js",
-    publicPath: "/dist"
+    entry: "./src/index.js",
+    devtool: "inline-source-map",
+    output: {
+      path: path.resolve(__dirname, "dist"),
+      filename: "bundle.js",
+      publicPath: "/dist"
+    },
+    plugins: [
+      new webpack.ProvidePlugin({
+        THREE: "three",
+        CANNON: "cannon"
+      }),
+      new IntlPlugin({
+        search: ['./src/**/*.intl']
+      })
+    ]
   },
-  plugins: [
-    new webpack.ProvidePlugin({
-      THREE: "three",
-      CANNON: "cannon"
-    })
-  ]
-};
+  module: {
+    rules: [{
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+        query: {
+          presets: ['es2015', 'stage-0']
+        }
+      },
+      {
+        test: /\.(intl|i18n)$/i,
+        use: [
+          IntlPlugin.loader(),
+          'yaml-loader'
+        ]
+      },
+      { test: /\.css$/, use: "css" },
+      { test: /\.(eot|gif|jpg|png|svg|ttf|woff)$/, use: "url-loader?limit=100000" }
+    ]
+  };
